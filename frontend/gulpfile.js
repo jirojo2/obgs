f1bbc687d8046afab1cf5a309fa44f00bbc238a0
José Ignacio Rojo <jirojo2@gmail.com>
2015-11-26 19:35:44 +0100

    gulp for frontend adapt tempaltes to final db structure

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var rename = require("gulp-rename");
+var copy = require("gulp-copy");
+var minifyCss = require('gulp-minify-css');
 var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('templatecache', function() {
@@ -21,4 +24,45 @@ gulp.task('scripts', ['templatecache'], function() {
         .pipe(gulp.dest('public'))
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('styles', [], function() {
+    return gulp
+        .src([
+            'src/app.css'
+        ])
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(gulp.dest('public'));
+})
+
+gulp.task('bootstrap', [], function() {
+    return gulp
+        .src('bower_components/bootstrap/**/*')
+        .pipe(copy('public', { prefix: 1 }))
+});
+
+gulp.task('font-awesome', [], function() {
+    return gulp
+        .src('bower_components/font-awesome/**/*')
+        .pipe(copy('public', { prefix: 1 }))
+});
+
+gulp.task('dependencies', ['font-awesome', 'bootstrap'], function() {
+    return gulp
+        .src([
+            'bower_components/angular/angular.min.js',
+            'bower_components/angular-resource/angular-resource.min.js',
+            'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+            'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
+        ])
+        .pipe(concat('dependencies.min.js'))
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('index', [], function() {
+    return gulp
+        .src('src/app.html')
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('default', ['dependencies', 'scripts', 'styles', 'index']);
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,39 +1,44 @@
+angular.module('templates', []);
+
 var app = angular.module('obgs',
         [
                 'ui.router',
                 'ui.bootstrap',
-                'ngResource'
+                'ngResource',
+                'templates'
         ]
 )
 
-app.config(function($stateProvider, $urlRouterProvider) {
-    //
-    // For any unmatched url, redirect to /state1
-    $urlRouterProvider.otherwise("/");
-    //
-    // Now set up the states
-    $stateProvider
-        .state('dashboard', {
-            url: "/",
-            templateUrl: "src/dashboard.html",
-            controller: "DashboardCtrl"
-        })
-        .state('help', {
-            url: "/",
-            templateUrl: "src/dashboard.html",
-            controller: "DashboardCtrl"
-        })
-        .state('hosts', {
-            url: "/hosts",
-            templateUrl: "src/hosts/list.html",
-            controller: "HostListCtrl"
-        })
-        .state('scans', {
-            url: "/scans",
-            templateUrl: "src/scans/list.html",
-            controller: "ScanListCtrl"
-        })
-})
+app.config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('dashboard', {
+                url: "/",
+                templateUrl: "dashboard.html",
+                controller: "DashboardCtrl"
+            })
+            .state('help', {
+                url: "/",
+                templateUrl: "dashboard.html",
+                controller: "DashboardCtrl"
+            })
+            .state('hosts', {
+                url: "/hosts",
+                templateUrl: "hosts/list.html",
+                controller: "HostListCtrl"
+            })
+            .state('scans', {
+                url: "/scans",
+                templateUrl: "scans/list.html",
+                controller: "ScanListCtrl"
+            })
+    }
+])
 
 app.controller('DashboardCtrl', ['$scope',
     function($scope) {
