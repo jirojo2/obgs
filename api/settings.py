03cc3d5ea478d792254ed491421fee064af7d0d1
José Ignacio Rojo Rivero <jirojo2@gmail.com>
2015-11-28 01:06:03 +0100

    enable regex search in api

--- api/settings.py
+++ api/settings.py
@@ -5,6 +5,7 @@ MONGO_PORT = 27017
 MONGO_USERNAME = ''
 MONGO_PASSWORD = ''
 MONGO_DBNAME = 'obgs'
+MONGO_QUERY_BLACKLIST = ['$where']
 
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
